83c8bef3b31e4ea89a052afcaebbd2daa148a727
Pushkar Patel <pushkar.umrao@gmail.com>
2023-05-09 11:41:34 +0530

    new Work

--- Resolver/server.py
+++ Resolver/server.py
@@ -1,16 +1,30 @@
-from resolver import DoH
+from fastapi import FastAPI
+import requests
+import json
 
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+app = FastAPI()
 
-myDoH=DoH()
-app=FastAPI()
+@app.get("/")
+async def resolve_dns(name: str):
+    try:
+        # Send a DNS over HTTPS (DoH) query to a DoH server
+        url = f"https://cloudflare-dns.com/dns-query?name={name}&type=A"
+        headers = {"accept": "application/dns-json"}
+        response = requests.get(url, headers=headers)
 
-@app.get('/')
-def home(name:str=None, type:str=None):
-    if name is None:
-        return {"message":"this is a custom DoH Server"}
-    else:
-        ans=myDoH.resolve(name, type)
-        return JSONResponse(content=ans.json(), media_type='application/dns-json')
+        # Parse the DoH response
+        data = response.content.decode("iso-8859-1")
+        # print(type(data))
+        # print(data['status'])
+        # print("Data Status is"+data['Status'])
+        data=json.loads(data)
+        if data["Status"] == 0:
+            return data["Answer"]
+        else:
+            return "Error: Unable to resolve DNS"
+    except:
+        return "Error: Unable to resolve DNS"
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ pi-hole/README.md
@@ -0,0 +1,9 @@
+# Secure DNS using Pi-Hole
+## What is Pi-Hole?
+Pi-Hole is a network-wide ad-blocking service that operates as a Domain Name System (DNS) sinkhole. 
+
+Pi-Hole works by intercepting DNS requests  This results in a faster and more private browsing experience.
+
+It is typically used to block access to certain domain names, however it can also be used to redirect system DNS requests.
+
+## DNS in Linux Kernel.
